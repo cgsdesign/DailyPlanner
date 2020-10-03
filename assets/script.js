4e9899b8543256f5b4aa0b9ae33c39d246a8b414
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-10-03 18:34:26 -0500

    funny test to see if my time is updating using +love and +hate. It is

--- assets/script.js
+++ assets/script.js
@@ -16,29 +16,39 @@ var threePM = localStorage.getItem("3pm")
 var fourPM = localStorage.getItem("4pm")
 var fivePM = localStorage.getItem("5pm")
 
+//on load:
+//$( "#tabs" ).tabs(); 
+//document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+function refreshAtM_S(m,s) { refreshAt(0, 0, 5);}
+
+var pullTask = function () {
+  pulledTask = localStorage.getItem(parent,textIWant)
+}
+
 $(".btn").on("click", function() {
   // get the parent's id attribute
     var parent = $(this).closest('div').attr('id')
-
   //Get text I want from text field
     textIWant = $(this).siblings("#text").val()
-
-  //save
+  //save(has to be inside because parent and textIwant are inside- ask TA to confirm)
   var saveTask = function () {
     localStorage.setItem(parent, textIWant)   
   }
     saveTask()
   });
-
-  var pullTask = function () {
-    pulledTask = localStorage.getItem(parent,textIWant)
-  }
-
-//reload page every 5 minutes to ensure colors changing
-var CurrentTime = moment().format("H");
-console.log(CurrentTime)
-
-
+  
+CurrentTime = moment().format("H") + "hate";
+function updateClock2() {
+  console.log(CurrentTime)
+}
+setInterval(updateClock2, 3000);
+//reload page every 30 seconds to ensure colors changing
+function updateClock() {
+  CurrentTime = moment().format("H")+"love";
+  console.log(CurrentTime)
+  return(CurrentTime)
+}
+setInterval(updateClock, 1000);
 //switching classes
 // var currentHour = 2
 // var timeBar = 4
